94e1b115b2f008f50d79d38d63a7735852c1bc43
pr3dr00 <88636685+pr3dr00@users.noreply.github.com>
2025-06-21 13:02:40 -0300

    Update index.php

--- BDDomus/index.php
+++ BDDomus/index.php
@@ -9,7 +9,7 @@
 <a href="create.php">Add New User</a>
 <table border="1" cellpadding="10">
     <tr>
-        <th>ID</th><th>Name</th><th>Email</th><th>Actions</th>
+        <th>ID</th><th>Name</th><th>Email</th><th>Password</th><th>Actions</th>
     </tr>
     <?php
     $result = $conn->query("SELECT * FROM users");
@@ -19,6 +19,7 @@
         <td><?= $row['id'] ?></td>
         <td><?= htmlspecialchars($row['name']) ?></td>
         <td><?= htmlspecialchars($row['email']) ?></td>
+        <td><?= htmlspecialchars($row['password']) ?></td>
         <td>
             <a href="edit.php?id=<?= $row['id'] ?>">Edit</a>
             <a href="delete.php?id=<?= $row['id'] ?>" onclick="return confirm('Are you sure?')">Delete</a>
