7e5c9590a34c78795c9dcf9399bd5623266a76ac
NajuCuebas <ajfcuebas@gmail.com>
2025-06-25 16:44:32 -0300

    sera q agora o bd vai?

--- BDDomus/create.php
+++ BDDomus/create.php
@@ -1,26 +1,33 @@
-<?php include 'db.php'; ?>
-<!DOCTYPE html>
-<html>
-<head><title>Create User</title></head>
-<body>
-<h2>Create User</h2>
-<form method="POST" action="">
-    Name: <input type="text" name="name" required><br><br>
-    Email: <input type="email" name="email" required><br><br>
-    Password: <input type="password" name="password" required><br><br>
-    <button type="submit">Save</button>
-</form>
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $conn->real_escape_string($_POST['name']);
-    $email = $conn->real_escape_string($_POST['email']);
-    $password = $conn->real_escape_string($_POST['password']);
+include 'db.php';
 
-    $conn->query("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
-    header("Location: index.php");
-    exit;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name     = $conn->real_escape_string($_POST['name']);
+    $email    = $conn->real_escape_string($_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Criptografa a senha
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
+    } else {
+        echo "<p style='color:red;'>Erro: " . $conn->error . "</p>";
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastro de Usuário</title>
+</head>
+<body>
+    <h2>Cadastro</h2>
+    <form method="POST" action="">
+        Nome: <input type="text" name="name" required><br><br>
+        Email: <input type="email" name="email" required><br><br>
+        Senha: <input type="password" name="password" required><br><br>
+        <button type="submit">Cadastrar</button>
+    </form>
 </body>
 </html>
--- BDDomus/db.php
+++ BDDomus/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db   = 'crud_demo';
+$db   = 'user_db';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
